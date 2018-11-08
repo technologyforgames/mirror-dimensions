de118106b9c01bfb94cdc395c1fae70da8524e35
Lincy Ellermeijer <lincyellermeijer@gmail.com>
2018-11-08 21:15:58 +0100

    First basic and correctly working prototype done!
    (Added smooth follow script to the camera, tweaked movement and adjusted background color)

--- Assets/Resources/Scripts/CameraScript.cs
+++ Assets/Resources/Scripts/CameraScript.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CameraScript : MonoBehaviour {
+public class CameraScript : MonoBehaviour
+{
+    public float DampTime = 0.15f;
+    public Transform Target;
 
-    public Transform target;
+    private Camera camera;
+    private Vector3 velocity = Vector3.zero;
 
-	void LateUpdate()
+    // Use this for initialization
+    void Start()
     {
-        // To follow the target, set position of the camera equal to target position
-        transform.position = new Vector3(target.position.x, transform.position.y,transform.position.z);
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If there is a target, smoothly follow this
+        if (Target != null)
+        {
+            Vector3 point = camera.WorldToViewportPoint(Target.position);
+            Vector3 delta = Target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, DampTime);
+        }
+
     }
 }
