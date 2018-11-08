f5ef84ca26796ba19d54b89dcd093f642e14e35e
Lincy Ellermeijer <lincyellermeijer@gmail.com>
2018-11-08 21:00:48 +0100

    Implemented background and added a basic camera follow script

---
+++ Assets/Resources/Scripts/CameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+
+	void LateUpdate()
+    {
+        // To follow the target, set position of the camera equal to target position
+        transform.position = new Vector3(target.position.x, transform.position.y,transform.position.z);
+    }
+}
