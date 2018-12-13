d17205341febbb7621ce6140cd0602bb2c897173
Lincy Ellermeijer <lincyellermeijer@gmail.com>
2018-12-13 21:40:11 +0100

    When blue key is picked up, blue wall will be destroyed

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -7,5 +7,9 @@ public class Key : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col) {
         Debug.Log("Key was picked up");
         Destroy(this.gameObject);
+
+        if (this.gameObject.tag == "BlueKey") {
+            Destroy(GameObject.FindWithTag("BlueWall"));
+        }
     }
 }
