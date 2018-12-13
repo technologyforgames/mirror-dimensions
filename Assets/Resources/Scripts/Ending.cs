a426c993503a8726d34b35811cbdf4ea0c4ec52a
Lincy Ellermeijer (MSI Laptop) <lincy_95@hotmail.com>
2018-12-13 15:48:28 +0100

    Level ending added

---
+++ Assets/Resources/Scripts/Ending.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour {
+
+    private bool playerEntered = false;
+    private bool mirrorPlayerEntered = false;
+
+    private void OnTriggerEnter2D(Collider2D target) {
+        if (target.tag == "Player") {
+            Debug.Log("Player entered");
+            playerEntered = true;
+        }
+        if (target.tag == "MirrorPlayer") {
+            Debug.Log("Mirror player entered");
+            mirrorPlayerEntered = true;
+        }
+    }
+
+    private void Update() {
+        if (playerEntered && mirrorPlayerEntered) {
+            SceneManager.LoadScene("Main");
+}
+    }
+}
