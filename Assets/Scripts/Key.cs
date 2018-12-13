9c3131796cca7fa9aae5f599b50cd627a60f3979
Lincy Ellermeijer (MSI Laptop) <lincy_95@hotmail.com>
2018-12-13 19:20:21 +0100

    Key script added

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("Key was picked up");
+        Destroy(this.gameObject);
+    }
+}
