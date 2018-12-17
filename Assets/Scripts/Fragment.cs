4eab24533df18b3a0a1fc8ba26816dc89ac52f32
Lincy Ellermeijer <lincyellermeijer@gmail.com>
2018-12-17 23:35:08 +0100

    Adjusted layers and added pickup object sound to end fragment

--- Assets/Scripts/Fragment.cs
+++ Assets/Scripts/Fragment.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Fragment : MonoBehaviour {
 
+    private AudioSource audioSource;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D target) {
         if (target.tag == "Player") {
+            audioSource.Play();
             LevelHandler.instance.LoadEnding();
         }
     }
