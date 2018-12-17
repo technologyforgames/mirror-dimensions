b54589df8050099620fb7d6f87ce88357f3d6b0f
Lincy Ellermeijer <lincyellermeijer@gmail.com>
2018-12-17 16:59:20 +0100

    Added audio to start button

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -6,7 +6,14 @@ using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour {
 
+    private AudioSource audioSource;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void LoadIntro() {
+        audioSource.Play();
         SceneManager.LoadScene("Intro");
     }
 
